d9457c4b44c0d36308938d71b4268539337e4434
Nuckles1 <danyxcharge@gmail.com>
2024-06-13 09:57:50 +0100

    main

--- furniture/src/app/components/Section.tsx
+++ furniture/src/app/components/Section.tsx
@@ -9,27 +9,27 @@ const Section = () => {
     return (
         <div className='w-full h-[100vh]'>
             <div className='h-full w-full max-sm:h-[100vh]'>
-           <BackgroundPlayer src='https://www.lyon-beton.com/wp-content/uploads/2024/04/VIDEO-16-9-HOME-1.webm' /> 
+           <BackgroundPlayer src='https://www.lyon-beton.com/wp-content/uploads/2024/04/VIDEO-16-9-HOME-1.webm' autoplay/> 
             </div>
             <div className='absolute w-full h-full justify-center text-center max-sm:top-0 top-10'>
                 <p className='bigi text-[143px] max-md:text-[85px] max-sm:text-[40px]'>NUCKLES DAAN</p>
-                <ul className='w-full items-center max-md:hidden justify-center text-center tiny flex'>
+                <ul className='w-full items-center justify-center text-center tiny flex'>
                     <li className='pt-5 cursor-pointer text-center'>SHOP</li>
                     <li className='ml-8 pt-5 cursor-pointer text-center'>ABOUT</li>
                     <li className='ml-8 pt-5 cursor-pointer text-center'>JOURNAL</li>
                     <li className='ml-8 pt-5 cursor-pointer text-center'>JOURNAL</li>
                 </ul>
-                <div className='flex w-full mt-14 max-md:mt-3 text-center justify-center'>
+                <div className='flex w-full mt-14 max-md:mt-8 text-center justify-center'>
                     <ul className='flex-row justify-center items-center flex flex-wrap gap-6 h-full w-[60%]'>
-                        <li className='text-4xl text-[#231f20] max-md:text-[55px] bold hov'><p className='tiny liney md:pb-3'> <br />PROJECT</p>NUCKLES DAAN</li>
-                        <li className='text-4xl text-[#231f20] max-md:text-[55px]  hov bold'><p className='tiny liney md:pb-3'> <br />COLLECTIONS</p>NEW DICE</li>
-                        <li className='text-4xl text-[#231f20] max-md:text-[55px]  hov bold'><p className='tiny liney md:pb-3'> <br />COLLECTIONS</p>NEW DICE</li>
-                        <li className='text-4xl text-[#231f20] max-md:text-[55px]  hov bold'><p className='tiny liney md:pb-3'> <br />COLLECTIONS</p>DICE</li>
-                        <li className='text-4xl text-[#231f20] max-md:text-[55px]  hov bold'><p className='tiny liney md:pb-3'> <br />COLLECTIONS</p>TWIST</li>
-                        <li className='text-4xl text-[#231f20] max-md:text-[55px]  hov bold'><p className='tiny liney md:pb-3'> <br />COLLECTIONS</p>TWIST</li>
-                        <li className='text-4xl text-[#231f20] max-md:text-[55px]  hov bold'><p className='tiny liney md:pb-3'> <br />COLLECTIONS</p>TWIST</li>
-                        <li className='text-4xl text-[#231f20] max-md:text-[55px]  hov bold'><p className='tiny liney md:pb-3'> <br />SESSIONS</p>STRUT RUST</li>
-                        <li className='text-4xl text-[#231f20] max-md:text-[55px]  hov bold'><p className='tiny liney md:pb-3'> <br />COLABBB</p>SOFFFA</li>
+                        <li className='text-4xl text-[#231f20] max-md:text-[30px] bold hov'><p className='tiny liney md:pb-3'> <br />PROJECT</p>NUCKLES DAAN</li>
+                        <li className='text-4xl text-[#231f20] max-md:text-[30px]  hov bold'><p className='tiny liney md:pb-3'> <br />COLLECTIONS</p>NEW DICE</li>
+                        <li className='text-4xl text-[#231f20] max-md:text-[30px]  hov bold'><p className='tiny liney md:pb-3'> <br />COLLECTIONS</p>NEW DICE</li>
+                        <li className='text-4xl text-[#231f20] max-md:text-[30px]  hov bold'><p className='tiny liney md:pb-3'> <br />COLLECTIONS</p>DICE</li>
+                        <li className='text-4xl text-[#231f20] max-md:text-[30px]  hov bold'><p className='tiny liney md:pb-3'> <br />COLLECTIONS</p>TWIST</li>
+                        <li className='text-4xl text-[#231f20] max-md:text-[30px]  hov bold'><p className='tiny liney md:pb-3'> <br />COLLECTIONS</p>TWIST</li>
+                        <li className='text-4xl text-[#231f20] max-md:text-[30px]  hov bold'><p className='tiny liney md:pb-3'> <br />COLLECTIONS</p>TWIST</li>
+                        <li className='text-4xl text-[#231f20] max-md:text-[30px]  hov bold'><p className='tiny liney md:pb-3'> <br />SESSIONS</p>STRUT RUST</li>
+                        <li className='text-4xl text-[#231f20] max-md:text-[30px]  hov bold'><p className='tiny liney md:pb-3'> <br />COLABBB</p>SOFFFA</li>
                     </ul>
                 </div>
             </div>
