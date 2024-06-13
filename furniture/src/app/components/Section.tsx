9b88605d2ffb8cb2e2f32f1d795e24483d8134fa
Nuckles1 <danyxcharge@gmail.com>
2024-06-13 11:21:10 +0100

    main

--- furniture/src/app/components/Section.tsx
+++ furniture/src/app/components/Section.tsx
@@ -12,7 +12,7 @@ const Section = () => {
                 <BackgroundVideo src='https://www.lyon-beton.com/wp-content/uploads/2024/04/VIDEO-16-9-HOME-1.webm' className='max-sm:h-[100vh]' autoPlay muted loop/>
             </div>
             <div className='absolute w-full h-full justify-center text-center max-sm:top-0 top-10'>
-                <p className='bigi text-[143px] max-md:text-[73px] max-sm:text-[40px]'>NUCKLES DAAN</p>
+                <p className='bigi text-[143px] max-md:text-[63px] max-sm:text-[40px]'>NUCKLES DAAN</p>
                 <ul className='w-full items-center max-md:mt-[-1rem] justify-center text-center tiny flex'>
                     <li className='pt-5 cursor-pointer text-center'>SHOP</li>
                     <li className='ml-8 pt-5 cursor-pointer text-center'>ABOUT</li>
@@ -20,7 +20,7 @@ const Section = () => {
                     <li className='ml-8 pt-5 cursor-pointer text-center'>JOURNAL</li>
                 </ul>
                 <div className='flex w-full mt-14 max-md:mt-4 text-center justify-center'>
-                    <ul className='flex-row justify-center items-center flex flex-wrap gap-6 h-full w-[60%] max-md:w-[80%]'>
+                    <ul className='flex-row justify-center items-center flex flex-wrap gap-6 h-full w-[60%] max-sm:w-[60%] max-md:w-[80%]'>
                         <li className='text-4xl text-[#231f20] max-md:text-[30px] bold hov'><p className='tiny liney md:pb-3'> <br />PROJECT</p>NUCKLES DAAN</li>
                         <li className='text-4xl text-[#231f20] max-md:text-[30px]  hov bold'><p className='tiny liney md:pb-3'> <br />COLLECTIONS</p>NEW DICE</li>
                         <li className='text-4xl text-[#231f20] max-md:text-[30px]  hov bold'><p className='tiny liney md:pb-3'> <br />COLLECTIONS</p>NEW DICE</li>
