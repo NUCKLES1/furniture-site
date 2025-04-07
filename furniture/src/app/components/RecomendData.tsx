9bd05d5762bd85520f97a05f1e165ffa0098a9f3
Nuckles1 <danyxcharge@gmail.com>
2025-04-07 15:35:47 +0100

    main

--- furniture/src/app/components/RecomendData.tsx
+++ furniture/src/app/components/RecomendData.tsx
@@ -9,7 +9,7 @@ const RecomendData = ({ product }: { product: Product }) => {
     return (
       <div>
         <div className="text-left pt-4 pl-2 text-[#231f20] cont">
-          <p className="liney max-sm:text-[25px]">{product?.title}</p>
+          <p className="liney max-sm:font-normal max-sm:text-[25px]">{product?.title}</p>
           <p className="text-[12px] max-sm:text-[15px] mt-2">{product?.description}</p>
         </div>
   
@@ -35,7 +35,7 @@ const RecomendData = ({ product }: { product: Product }) => {
           )}
         </div>
         <div className="flex max-sm:mt-8 -mt-2 text-[#231f20] conti ml-2">
-          <div className="text-[12px] font-bold max-sm:text-[15px] mt-8">
+          <div className="text-[12px] lg:font-bold max-sm:text-[15px] mt-8">
             <PriceView price={product?.price} />
           </div>
           <p className="ml-2 max-sm:text-[15px] text-[12px] mt-8">{product?.brand}</p>
